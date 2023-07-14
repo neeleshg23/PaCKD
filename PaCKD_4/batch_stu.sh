cafed9e7db9707d8f1682f8ff86ec8c46b26fb0e
Neel Gupta <neeleshgupta023@gmail.com>
2023-07-14 15:57:36 -0700

    adding some new test results + conda env

---
+++ PaCKD_4/batch_stu.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+APPS=("pr-3.txt.xz" "sssp-3.txt.xz") 
+GPUS=("0" "1" "2" "3" "4" "5")
+MODELS=("l" "m" "r")
+CLUSTERS=("a" "d" "i") 
+ALPHAS=("0.1" "0.3" "0.7" "0.9")
+
+gpu_index=0
+
+for cluster in "${CLUSTERS[@]}"; do
+    for app1 in "${APPS[@]}"; do
+        for model in "${MODELS[@]}"; do
+            for alpha in "${ALPHAS[@]}"; do 
+                GPU="${GPUS[$gpu_index]}"
+                python src/train_stu.py $app1 $cluster $alpha $alpha $alpha $alpha $model $model $model $model $model $GPU
+            done 
+        done
+    done
+done
